4bd2b7aafe4b823e44e26ed774975070050491e3
Dmitrij <gdems@operamail.com>
2021-05-23 20:07:44 +0300

    server done

--- chat_server.py
+++ chat_server.py
@@ -5,6 +5,8 @@ from sys import argv
 from argparse import ArgumentParser
 import logging
 import log.server_log_config
+import select
+
 
 logger = logging.getLogger('server_app')
 
@@ -16,33 +18,92 @@ def log(func):
     return deco
 
 @log
+def new_listen_socket(sock_parms):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((sock_parms.address, int(sock_parms.port)))
+    sock.listen(5)
+    sock.settimeout(0.2)
+    return sock
+
+@log
 def create_parser():
     parser = ArgumentParser()
     parser.add_argument('-p', '--port', default=7777)
     parser.add_argument('-a', '--address', default='')
     return parser
 
-def main():
-    parser = create_parser()
-    arg = parser.parse_args(argv[1:])
+def read_requests(r_clients, all_clients):
+    """ Чтение запросов из списка клиентов
+    """
+    requests = {}  # Словарь запросов клиентов вида {сокет: запрос}
 
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind((arg.address, int(arg.port)))
-    s.listen(5)
+    for sock in r_clients:
+        try:
+            requests[sock] = pickle.loads(sock.recv(640))
+        except:
+            print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
+            all_clients.remove(sock)
+    return requests
+
+@log
+def write_responses(requests, w_clients, all_clients):
+    """ Эхо-ответ сервера клиентам, от которых были запросы
+    """
+
+    for sock in w_clients:
+        if sock in requests:
+            try:
+                # Подготовить и отправить ответ сервера
+                if requests[sock]['action'] == 'presence':
+                    logger.info(f'presence message received from client {sock.getpeername()}')
+                    response = {
+                        "response": 202,
+                        "time": time.time(),
+                        "alert": "chat-server confirm connection"
+                    }
+                    sock.send(pickle.dumps(response))
+
+            except:  # Сокет недоступен, клиент отключился
+                print('Клиент {} {} отключился'.format(sock.fileno(), sock.getpeername()))
+                sock.close()
+                all_clients.remove(sock)
+
+    for sock in requests:
+        if requests[sock]['action'] == 'msg':
+            response = requests[sock]
+            for receiver in all_clients:
+                receiver.send(pickle.dumps(response))
+
+
+def main():
+    arg = create_parser().parse_args(argv[1:])
+    s = new_listen_socket(arg)
+    clients = []
+
 
     while True:
-        client, addr = s.accept()
-        data = pickle.loads(client.recv(640))
-        if data['action'] == 'presence':
-            logger.info('presence message received from client %s', addr[0])
-            response = {
-                "response": 202,
-                "time": time.time(),
-                "alert": "chat-server confirm connection"
-            }
-            client.send(pickle.dumps(response))
+        try:
+            conn, addr = s.accept()  # Проверка подключений
+        except OSError as e:
+            pass                        # timeout вышел
+        else:
+            print(f'Получен запрос на соединение с {str(addr)}')
+            clients.append(conn)
+        finally:
+            # Проверить наличие событий ввода-вывода без таймаута
+            wait = 3
+            r = []
+            w = []
+            try:
+                r, w, e = select.select(clients, clients, [], wait)
+            except Exception as e:
+            # Исключение произойдет, если какой-то клиент отключится
+                pass        # Ничего не делать, если какой-то клиент отключился
+            
+            requests = read_requests(r, clients)  # Сохраним запросы клиентов
+            if requests:
+                write_responses(requests, w, clients)  # Выполним отправку ответов клиентам
 
-        client.close()
 
 
 if __name__ == '__main__':
